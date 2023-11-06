8c82ddeb45e6f901eec46e27e7a354649f97b264
angjoagos <127690253+angjoagos@users.noreply.github.com>
2023-11-06 11:33:25 -0600

    update

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "1f6438c412fd0bb6a319895c5eec58b9"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "5b0ead2dd8e94880a75c36e79cdb677c"
